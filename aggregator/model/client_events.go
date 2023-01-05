0cd305d86dfdbb995297b4494331e17b65f3cf89
Junmin Ahn <junminahn@outlook.com>
2023-01-05 11:48:23 -0800

    chore: run aggregator server in multi-thread with bigger connection pool

--- aggregator/model/client_events.go
+++ aggregator/model/client_events.go
@@ -15,12 +15,9 @@ type ClientEvent struct {
 }
 
 func UpsertClientEvent(environment string, realmID string, clientID string, eventType string, date time.Time) (*ClientEvent, error) {
-	db := ConnectDB()
-	defer db.Close()
-
 	var clientEvent ClientEvent
 	query := "INSERT INTO client_events (environment, realm_id, client_id, event_type, date, count) VALUES(?,?,?,?,?,1) ON CONFLICT (environment, realm_id, client_id, event_type, date) DO UPDATE SET count = client_events.count + 1"
-	_, err := db.Query(&clientEvent, query, environment, realmID, clientID, eventType, date)
+	_, err := pgdb.Query(&clientEvent, query, environment, realmID, clientID, eventType, date)
 
 	if err != nil {
 		log.Println(err)
