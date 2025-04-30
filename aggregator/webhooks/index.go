da93f1c1dc3e9f60915ad19e806b18f6c4c9ba64
jonathan langlois <jonathan.langlois@gov.bc.ca>
2025-04-30 15:54:32 -0700

    fix: rocketchat err
    handle rocketchat error gracefully

--- aggregator/webhooks/index.go
+++ aggregator/webhooks/index.go
@@ -10,7 +10,7 @@ import (
 )
 
 type RocketChatNotifier interface {
-	NotifyRocketChat(text string, title string, body string)
+	NotifyRocketChat(text string, title string, body string) error
 }
 
 type RocketChat struct{}
@@ -18,7 +18,7 @@ type RocketChat struct{}
 /*
 For the RC webhook, text will appear at the top, with a title and collapsible body below.
 */
-func (r *RocketChat) NotifyRocketChat(text string, title string, body string) {
+func (r *RocketChat) NotifyRocketChat(text string, title string, body string) error {
 	// HTTP endpoint
 	log.Println("Sending rocket chat notification")
 	posturl := utils.GetEnv("RC_WEBHOOK", "")
@@ -37,21 +37,25 @@ func (r *RocketChat) NotifyRocketChat(text string, title string, body string) {
 	requestBody := []byte(fmt.Sprintf(requestBodyTemplate, text, title, body))
 
 	// Create a HTTP post request
-	resp, err := http.NewRequest("POST", posturl, bytes.NewBuffer(requestBody))
+	req, err := http.NewRequest("POST", posturl, bytes.NewBuffer(requestBody))
 	if err != nil {
 		log.Println("Error sending rocket chat notification", err)
+		return err
 	}
-	resp.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Type", "application/json")
 
 	client := &http.Client{}
-	res, err := client.Do(resp)
+	res, err := client.Do(req)
 
 	if err != nil {
 		log.Println("Error sending rocket chat notification", err)
+		return err
 	}
-	if res.StatusCode != 200 {
+	if res.StatusCode != http.StatusOK {
 		log.Println("Error sending rocket chat notification", res.Status, res.Body)
+		return fmt.Errorf("received non-200 response: %s", res.Status)
 	}
 
 	defer res.Body.Close()
+	return nil
 }
